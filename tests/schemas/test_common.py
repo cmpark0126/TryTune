511efc58f203cb4547b2dd2157819780a3f19d5c
Chunmyong Park <chunmyong.park@kaist.ac.kr>
2023-06-09 00:34:08 +0900

    Send shape also to infer

--- tests/schemas/test_common.py
+++ tests/schemas/test_common.py
@@ -8,7 +8,7 @@ def test_infer_schema() -> None:
     valid_data = {
         "target": "pipe1",
         "inputs": {
-            "i1": {"data": [1, 2, 3]},
+            "i1": {"data": [1, 2, 3], "shape": [1, 3]},
             "i2": {"data": [4, 5, 6]},
         },
     }
@@ -23,8 +23,11 @@ def test_infer_schema() -> None:
     assert infer.target == "pipe1"
     assert "i1" in infer.inputs
     assert infer.inputs["i1"].data == [1, 2, 3]
+    assert infer.inputs["i1"].shape is not None
+    assert infer.inputs["i1"].shape == [1, 3]
     assert "i2" in infer.inputs
     assert infer.inputs["i2"].data == [4, 5, 6]
+    assert infer.inputs["i2"].shape is None
 
     invalid_data = {
         "target": "pipe1",
--- trytune/schemas/common.py
+++ trytune/schemas/common.py
@@ -1,10 +1,11 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class DataSchema(BaseModel):
     data: List[Any]
+    shape: Optional[List[int]] = None
 
 
 class InferSchema(BaseModel):
@@ -19,8 +20,8 @@ class InferSchema(BaseModel):
         {
             "target": "pipe1",
             "inputs": [
-                "i1": {"data": [1.0, 2.0, 3.0]},
-                "i2": {"data": [1.0, 2.0, 3.0]},
+                "i1": {"data": [1.0, 2.0, 3.0], "shape": [3, 224, 224]},
+                "i2": {"data": [1.0, 2.0, 3.0], "shape": [3, 224, 224]},
             ]
         }
     """
